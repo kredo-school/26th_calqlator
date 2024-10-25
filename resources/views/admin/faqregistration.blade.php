f1c3665a40d2bf56320174dd3c5944355d1a0d35
uchijun <uchijun621@gmail.com>
2024-10-25 22:26:32 +0900

    Rename

--- resources/views/admin/faqregistration.blade.php
+++ resources/views/admin/faqregistration.blade.php
@@ -3,20 +3,30 @@
 @section('title', 'Admin: FAQList')
 
 @section('content')
-<form action="#">
-    <p>
-        FAQ Registration  <i class="fa-solid fa-circle-plus text-danger"></i>
-    </p>
-    <div class="row">
-        <div class="col-6">
-            <label for="question" class="form-label fw-bold">Question</label>
-            <textarea name="question" id="question" rows="6" class="form-control"></textarea>
+    <form action="#">
+        <div class="row my-3">
+            <div class="col-2"></div>
+            <div class="col-4 fw-bold">
+                FAQ Registration  <i class="fa-solid fa-circle-plus text-danger"></i>
+            </div>
         </div>
-        <div class="col-6">
-            <label for="answer" class="form-label fw-bold">Answer</label>
-            <textarea name="answer" id="answer" rows="6" class="form-control"></textarea>
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-4">
+                <label for="question" class="form-label">Question</label>
+                <textarea name="question" id="question" rows="6" class="form-control"></textarea>
+            </div>
+            <div class="col-4">
+                <label for="answer" class="form-label">Answer</label>
+                <textarea name="answer" id="answer" rows="6" class="form-control"></textarea>
+            </div>
         </div>
-    </div>
-    <button type="submit" class="btn btn-primary px-3">Save</button>
-</form>
+        <div class="row ">
+            <div class="col-2"></div>
+            <div class="col-4"></div>
+            <div class="col-4 text-end mt-3">
+                <button type="submit" class="btn text-success btn-outline-success px-4">Save</button>
+            </div>
+        </div>
+    </form>
 @endsection
