78a2473a47f9d100b55832d26fb38d95d556b166
Ayaka Mamada <kissy.missy.uvrwrld@gmail.com>
2024-10-31 19:48:09 +0900

    updated some part of file done with: ・front end still need: ・sort function

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Admin / Food & Exercise Confirmation
-Route::get('/admin/food/confirmation',[ConfirmationController::class, 'index'])->name('admin.food.confirmation');
-Route::patch('/admin/food/confirmation/{id}',[ConfirmationController::class, 'confirm'])->name('admin.food.confirm');
-Route::delete('/admin/food/confirmation/delete/{id}',[ConfirmationController::class, 'delete'])->name('admin.food.delete');
+Route::get('/admin/food/confirmation',[ConfirmationController::class, 'food_index'])->name('admin.food.confirmation');
+Route::get('/admin/exercise/confirmation',[ConfirmationController::class, 'exercise_index'])->name('admin.exercise.confirmation');
+Route::patch('/admin/confirmation/confirm/{id}',[ConfirmationController::class, 'confirm'])->name('admin.confirm');
+Route::delete('/admin/confirmation/delete/{id}',[ConfirmationController::class, 'delete'])->name('admin.delete');
