4d353262d55abb977911a5d9da26add1ce0b6ff0
Ayaka Mamada <kissy.missy.uvrwrld@gmail.com>
2024-11-06 20:02:36 +0900

    still in progress
    updated:
    -web.php
    -ConfirmationController

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,89 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Admin / Food & Exercise Confirmation
-Route::get('/admin/food/confirmation',[ConfirmationController::class, 'index'])->name('admin.food.confirmation');
+Route::get('/admin/food/confirmation',[ConfirmationController::class, 'food_index'])->name('admin.food.confirmation');
+Route::get('/admin/exercise/confirmation',[ConfirmationController::class, 'exercise_index'])->name('admin.food.confirmation');
 Route::patch('/admin/food/confirmation/{id}',[ConfirmationController::class, 'confirm'])->name('admin.food.confirm');
 Route::delete('/admin/food/confirmation/delete/{id}',[ConfirmationController::class, 'delete'])->name('admin.food.delete');
