3c705f54f5d66420a1dea7a36e7b2ee8e67b4eb9
Ayaka Mamada <kissy.missy.uvrwrld@gmail.com>
2024-11-07 22:04:10 +0900

    still in progress
    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConfirmationController;
-use App\Http\Controllers\FaqController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -58,6 +58,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 // FAQ 
+use App\Http\Controllers\FaqController;
 Route::get('/faq', [FaqController::class, 'index'])->name('user.faq');
 
 
