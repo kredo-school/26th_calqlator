1beb6de637bf8eff36e71b10e00d97b4705d62fc
Ayaka Mamada <kissy.missy.uvrwrld@gmail.com>
2024-11-08 21:02:29 +0900

    still adjusting colors and opacity. changed: -background image -background color of Q&A box

--- public/js/faq.js
+++ public/js/faq.js
@@ -1,7 +1,6 @@
 'use strict';
 {
-    const dts = document.querySelectorAll('dt'); //dtを全て取得
-
+    const dts = document.querySelectorAll('dt'); 
     dts.forEach(dt => {
         dt.addEventListener('click', () => {
             dt.parentNode.classList.toggle('appear');
