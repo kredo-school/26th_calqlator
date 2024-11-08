9d3fd7afb3a9842239dc6c48958aa144571a6f48
uchijun <uchijun621@gmail.com>
2024-11-08 22:09:57 +0900

    still in progress

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/admin/food/confirmation',[ConfirmationController::class, 'food_inde
 Route::get('/admin/exercise/confirmation',[ConfirmationController::class, 'exercise_index'])->name('admin.exercise.confirmation');
 Route::patch('/admin/confirmation/confirm/{id}',[ConfirmationController::class, 'confirm'])->name('admin.confirm');
 Route::delete('/admin/confirmation/delete/{id}',[ConfirmationController::class, 'delete'])->name('admin.delete');
+
+Route::get('/admin/faqlist/index', [FAQController::class, 'index'])->name('admin.faqlist.index');
