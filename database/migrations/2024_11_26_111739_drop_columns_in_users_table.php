a3bb679895f3919188c2d0e918ec16f65c9b55b4
saki <rst1455@gmail.com>
2024-11-28 21:31:47 +0900

    Pull requests have been made for changes to the table. For everyone's sake, please approve it.

--- database/migrations/2024_11_26_111739_drop_columns_in_users_table.php
+++ database/migrations/2024_11_26_111739_drop_columns_in_users_table.php
@@ -12,13 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('email');
-            $table->dropColumn('password');
-            $table->dropColumn('remember_token');
-            $table->dropColumn('created_at');
-            $table->dropColumn('updated_at');
+            $table->dropColumn('name');
+            $table->dropColumn('email_verified_at');
       
-            $table->string('email')->unique();;
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username')->unique();
+            $table->longtext('avatar')->nullable();
+            $table->string('email');
             $table->string('name');
             $table->string('password');
             $table->integer('role_id')->default(2)->comment('1:admin | 2:user');
