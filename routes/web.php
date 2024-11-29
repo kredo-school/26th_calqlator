e5076cb49212d4187bd14a49cf68f3b8d2bda89e
Ayaka Mamada <kissy.missy.uvrwrld@gmail.com>
2024-11-29 21:59:09 +0900

    done with frontend and backend(mostly) -Guest Home Page Guest Sign Up Page -User Login Page -User Change Password Pages -User Page / NavBar & Footer

--- routes/web.php
+++ routes/web.php
@@ -10,25 +10,19 @@ use App\Http\Controllers\ConfirmationController;
 use App\Http\Controllers\Admin\HomesController;
 use App\Http\Controllers\FaqController;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\PasswordResetController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/meals', [MealController::class, 'index']);
-Route::post('/meals', [MealController::class, 'store'])->name('meals.store');
-Route::get('/search', [MealController::class, 'search']);
-
-
-
-Route::get('/daily-condition', function () {
-    return view('daily_condition');
-});
+// Guest & User
+Route::get('/', function () {return view('home');})->name('home');
+Route::post('/find/reset/user', [PasswordResetController::class, 'findResetUser'])->name('find.user.reset.id');
+Route::post('/update/password/{id}', [PasswordResetController::class, 'update'])->name('update.password');
 
 // User / Home Page
 Route::get('/user/home', [HomePageController::class, 'index'])->name('user.home');
@@ -39,6 +33,13 @@ Route::get('/user/calendar/info/{date}', [CalendarController::class, 'everydayIn
 Route::get('/faq', [FaqController::class, 'index'])->name('user.faq');
 // User / ChatPage
 Route::get('/chatpage/index', [ChatController::class, 'userindex'])->name('user.chatpage.index');
+//User / Meal Registration
+Route::get('/meals', [MealController::class, 'index']);
+Route::post('/meals', [MealController::class, 'store'])->name('meals.store');
+Route::get('/search', [MealController::class, 'search']);
+//User / Daily Condition
+Route::get('/daily-condition', function () {return view('daily_condition');});
+
 // ADMIN
 // Route::group(['prefix' => 'admin', 'as' => 'admin.' , 'middleware' => 'admin'], function(){}
 // Admin / Homepage
