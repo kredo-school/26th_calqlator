83780fbcc7f77b974221d0585b35783636790848
Ayaka Mamada <kissy.missy.uvrwrld@gmail.com>
2024-12-14 17:15:51 +0900

    adjusted middleware in web.php added href to <a> in navbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
+        Gate::define('admin', function($user){
+            return $user->role_id == User::ADMIN_ROLE_ID;
+        });
     }
 }
